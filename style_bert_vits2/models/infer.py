8ae183391c977c10d6e5625749acdd7a5346c011
q9uri <9ri.nasu@gmail.com>
2026-03-23 10:59:01 +0900

    suport onnx local attn

--- style_bert_vits2/models/infer.py
+++ style_bert_vits2/models/infer.py
@@ -14,7 +14,7 @@ from style_bert_vits2.models import commons, utils
 from style_bert_vits2.models.hyper_parameters import HyperParameters
 from style_bert_vits2.models.infer_onnx import TokenDurationsResult
 from style_bert_vits2.models.models import SynthesizerTrn
-from style_bert_vits2.models.models_jp_extra import (
+from style_bert_vits2.models.models_jp2_extra import (
     SynthesizerTrn as SynthesizerTrnJPExtra,
 )
 from style_bert_vits2.models.models_nanairo import (
