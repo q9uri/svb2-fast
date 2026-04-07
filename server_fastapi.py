8aea0e2fd49a3d9198b2c0fe5f9ad051c433b742
quiri <9ri.nasu@gmail.com>
2026-04-07 14:50:21 +0900

    fix sever_fastapi.py

--- server_fastapi.py
+++ server_fastapi.py
@@ -34,7 +34,7 @@ from style_bert_vits2.constants import (
 )
 from style_bert_vits2.logging import logger
 from style_bert_vits2.nlp import bert_models, onnx_bert_models
-import pyopenjtalk
+from style_bert_vits2.nlp.japanese import pyopenjtalk_worker as pyopenjtalk
 from style_bert_vits2.nlp.japanese.g2p_utils import g2kata_tone
 from style_bert_vits2.nlp.japanese.normalizer import normalize_text
 from style_bert_vits2.nlp.japanese.user_dict import update_dict
